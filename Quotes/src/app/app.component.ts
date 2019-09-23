93fd74e4a5d56cab412632d724d24f3bc1454e95
Nkurumwa <nkurumwa51@gmail.com>
2019-09-23 11:43:59 +0300

    fixes new quotes

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -8,12 +8,9 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes:Quote []= [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    new Quote(1, 'first', 'Only those who will risk going too far can possibly find out how far one can go',new Date(2019,10,10)),
+    new Quote(2,'Candle','A candle loses nothing when lighting another candle',new Date(2019,11,12)),
+    new Quote(3,'Motivation','Motivation gets you moving and determination keeps you going',new Date(2020,12,12)),
   ];
 
 }
