d3601adee99dda2d48682fc2b33681e4bf9f974e
Nkurumwa <nkurumwa51@gmail.com>
2019-09-23 11:46:35 +0300

    adds days the quotes were added

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -10,7 +10,6 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'first', 'Only those who will risk going too far can possibly find out how far one can go',new Date(2019,10,10)),
     new Quote(2,'Candle','A candle loses nothing when lighting another candle',new Date(2019,11,12)),
     new Quote(3,'Motivation','Motivation gets you moving and determination keeps you going',new Date(2020,12,12)),
-
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
